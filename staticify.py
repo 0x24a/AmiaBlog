622828cad216f805d4e1afec1f30c00e79b900c7
0x24a <miku399993@gmail.com>
2026-01-12 12:23:32 +0800

    fix(staticify): use correct template name in friend links rendering

--- staticify.py
+++ staticify.py
@@ -97,7 +97,7 @@ class AmiaBlogStaticGenerator:
             logger.info("\tRendering: friend-links.html")
             self.renderer.render_static(
                 os.path.join(self.destination, "friend-links.html"),
-                "friend-links.html",
+                "friend_links.html",
                 friend_links=self.config.friend_links,
             )
         logger.info("\tRendering: 404.html (need extra configuration to work)")
